bf6fe1c9d5e962f993f203d51b3b8003d218edfd
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2022-10-11 14:20:12 +0530

    [Added] backdrop [Changed] BG Image

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="main">
-      <h1 className="temp">281.94</h1>
-      <h2 className="temp">Clouds</h2>
+    <div className="backdrop">
+      <div className="main">
+        <h1 className="temp">281.94</h1>
+        <h2 className="temp">Clouds</h2>
+      </div>
     </div>
   );
 }
